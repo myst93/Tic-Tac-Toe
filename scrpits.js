201e4be4b06b49c2567a16c5949d1755f89c76d1
myst93 <parth.oberai39@gmail.com>
2025-07-03 15:30:01 +0530

    Yet to enchance the code

--- scrpits.js
+++ scrpits.js
@@ -1,11 +1,15 @@
 let boxes = document.querySelectorAll(".boxes");
+let reset = document.querySelector(".reset")
+let newgame = document.querySelector(".newgame")
+let msgcontainer = document.querySelector(".msg-container")
+let msg = document.querySelector("#msg")
 let turn0=true;
 
-const win =[
+const win = [
     [0,1,2],
     [0,4,8],
     [0,3,6],
-    [3,5,6],
+    [3,4,5],
     [6,7,8],
     [1,4,7],
     [2,4,6],
@@ -13,7 +17,17 @@ const win =[
     
 ]
 
-
+const disabledboxes =()=>{
+    for(let box of boxes){
+        box.disabled=true
+    }
+}
+const enabledboxes =()=>{
+    for(let box of boxes){
+        box.disabled=false
+        box.innerText=""
+    }
+}
 boxes.forEach((val)=>{
     val.addEventListener("click", ()=>{
         if(turn0===true){
@@ -27,9 +41,21 @@ boxes.forEach((val)=>{
 
         checkwinner();
     })
+
 })
+const resetGame =()=>{
+    turn0=true
+    enabledboxes()
+    msgcontainer.classList.add("hide")
+    
+}
 
 
+const showwinner =(winner)=>{
+    msg.innerText=`Congratulations the winner is ${winner}`
+    msgcontainer.classList.remove("hide")
+    disabledboxes()
+}
 const checkwinner = ()=>{
     for(let i of win ){
         let var0=boxes[i[0]].innerText
@@ -38,9 +64,15 @@ const checkwinner = ()=>{
 
         if(var0 !="" && var1 != "" && var2 !=""){
             if(var0==var1 && var1==var2){
-                console.log()
-                
-            }
-    }   }
+                console.log(`Winner is ${var0}`)
+                showwinner(var0);
+                // disabledboxes();   
+            } 
+        } 
+    }   
 
 }
+
+
+reset.addEventListener("click", resetGame)
+newgame.addEventListener("click", resetGame)
