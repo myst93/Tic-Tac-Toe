6f837694c8513bce37d289c9f6dee66304cf5283
myst93 <parth.oberai39@gmail.com>
2025-07-03 18:57:47 +0530

    Still working on it

--- scrpits.js
+++ scrpits.js
@@ -17,18 +17,7 @@ const win = [
     
 ]
 
-const disabledboxes =()=>{
-    for(let box of boxes){
-        box.disabled=true
-    }
-}
-const enabledboxes =()=>{
-    for(let box of boxes){
-        box.disabled=false
-        box.innerText=""
-    }
-}
-boxes.forEach((val)=>{
+boxes.forEach((val)=>{                // callbackfunction is passed as an argument
     val.addEventListener("click", ()=>{
         if(turn0===true){
             val.innerText="X"
@@ -38,33 +27,26 @@ boxes.forEach((val)=>{
             turn0=true
         }
         val.disabled =true;
-
         checkwinner();
     })
-
-})
-const resetGame =()=>{
-    turn0=true
-    enabledboxes()
-    msgcontainer.classList.add("hide")
     
-}
+})
 
+// Function to check for a winner
+// It checks all winning combinations to see if any player has won
 
-const showwinner =(winner)=>{
-    msg.innerText=`Congratulations the winner is ${winner}`
-    msgcontainer.classList.remove("hide")
-    disabledboxes()
-}
 const checkwinner = ()=>{
     for(let i of win ){
-        let var0=boxes[i[0]].innerText
-        let var1=boxes[i[1]].innerText
-        let var2=boxes[i[2]].innerText
+        let var0=boxes[i[0]].innerText // Get the text inside the box
+        let var1=boxes[i[1]].innerText 
+        let var2=boxes[i[2]].innerText 
+        // var0 = boxes[0].innerText (top-left)
+        //var1 = boxes[4].innerText (center)
+        //var2 = boxes[8].innerText (bottom-right)
 
         if(var0 !="" && var1 != "" && var2 !=""){
             if(var0==var1 && var1==var2){
-                console.log(`Winner is ${var0}`)
+                // console.log(`Winner is ${var0}`)
                 showwinner(var0);
                 // disabledboxes();   
             } 
@@ -73,6 +55,33 @@ const checkwinner = ()=>{
 
 }
 
+const showwinner =(winner)=>{
+    msg.innerText=`Congratulations the winner is ${winner}`
+    msgcontainer.classList.remove("hide")
+    disabledboxes()
+}
+
+const disabledboxes =()=>{
+    for(let box of boxes){
+        box.disabled=true
+    }
+}
+
+const enabledboxes =()=>{
+    for(let box of boxes){
+        box.disabled=false
+        box.innerText=""
+    }
+}
+
+const resetGame =()=>{
+    turn0=true
+    enabledboxes()
+    msgcontainer.classList.add("hide")
+    
+}
+
+// Event listeners for reset and new game buttons
 
 reset.addEventListener("click", resetGame)
 newgame.addEventListener("click", resetGame)
